cb90b5d7029c5dd727df2db0c79bfa8dd52868a9
Cloud User <centos@ip-172-31-7-197.eu-west-2.compute.internal>
2020-06-10 14:44:43 +0000

    A complete solution to Ex4

--- Ex4/top.v
+++ Ex4/top.v
@@ -16,3 +16,35 @@
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module dice(
+	input rst,
+	input clk,
+	input button,
+	
+	output reg [2:0] throw
+	);
+	
+	always @(posedge clk)
+	begin
+		// reset
+		if(rst == 1)
+			throw <= 2'b000;
+		// rectify illegal values
+		else if(throw == 2'b000 || throw == 2'b111)
+			throw <= 2'b001;
+		// button pressed
+		else if(button)
+		begin
+			if(throw == 2'b110)
+				throw <= 2'b001;
+			else
+				throw <= throw + 1;
+		end
+		// button not pressed
+		else
+			throw <= throw;	
+	end
+	
+endmodule
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -7,3 +7,77 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+
+//Todo: Parameters
+	parameter CLK_PERIOD = 10;
+	
+//Todo: Registers and wires
+	reg rst = 0;
+	reg button = 0; 
+	reg clk = 0;
+	reg err = 0;
+	wire [2:0] throw = 2'b000;
+	
+//Todo: Clock generation
+	initial
+	begin
+		clk = 0;
+		forever
+			#(CLK_PERIOD/2) clk = ~clk;
+	end
+
+//Todo: User logic
+	initial
+	
+	begin
+		// test normal throw
+		button = 1;
+		#(CLK_PERIOD * 10)
+		if(throw != 2'b100)
+		begin
+        	$display("***THROW FAILED!***");    
+        	err=1;
+    	end
+		
+		// test reset
+		button = 0;
+		rst = 1;
+		#CLK_PERIOD
+		if(throw != 2'b000)
+		begin
+        	$display("***RESET FAILED!***");    
+        	err=1;
+    	end
+    	
+    	// test illegal values
+    	rst = 0;
+    	#CLK_PERIOD
+		if(throw == 2'b000 || throw == 2'b111)
+		begin
+        	$display("***ILLEGAL VALUE!***");    
+        	err=1;
+    	end
+		
+	end
+
+//Todo: Finish test, check for success
+	initial 
+	begin
+        #500 
+        if (err==0)
+        	$display("***TEST PASSED! x)***");
+        $finish;
+    end
+
+//Todo: Instantiate counter module
+	dice top ( 
+		.rst (rst),
+		.clk (clk),
+		.button (button),
+		.throw (throw));
+
+endmodule 
