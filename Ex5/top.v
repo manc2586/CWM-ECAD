8ce50e13dcf7a901a4651a4f965222c2f526c6a3
Cloud User <centos@ip-172-31-7-197.eu-west-2.compute.internal>
2020-06-11 10:58:24 +0000

    oof

--- Ex5/top.v
+++ Ex5/top.v
@@ -11,5 +11,58 @@
 //
 //  outputs:
 //           red, amber, green
+//
+//   Red  Amber Green
+//    x     x     x
+//
+//  Legal states:    100  110  001  010
+//  
+//  Illegal states:  000  011  101  111
+//
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module trafficlight(
+	input clk,
+	
+	output red,
+	output amber,
+	output green
+	);
+	
+	reg [2:0] state;
+
+	assign red = state[2];
+	assign amber = state[1];
+	assign green = state[0];
+
+	always @(posedge clk)
+	begin
+		case(state)
+			// red light
+			3'b100: begin
+				state <= 3'b110;
+				end
+			// red and orange
+			3'b110: begin
+				state <= 3'b001;
+				end
+			// green
+			3'b001: begin
+				state <= 3'b010;
+				end
+			// orange
+			3'b010: begin
+				state <= 3'b100;
+				end
+			// illegal states
+			default: begin
+				state <= 3'b100;
+				end
+		endcase
+	end
+	
+
+endmodule
+	
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -7,3 +7,82 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module top_tb(
+	);
+
+//Todo: Parameters
+	parameter CLK_PERIOD = 10;
+	
+//Todo: Registers and wires
+	reg [2:0] state;
+	reg err = 0;
+	reg clk;
+	wire red;
+	wire amber;
+	wire green;
+
+//Todo: Clock generation
+	initial
+	begin
+		clk = 0;
+		forever
+			#(CLK_PERIOD/2) clk = ~clk;
+	end
+
+//Todo: User logic
+    initial 
+    begin
+    // Test for valid states transition
+        #(3*CLK_PERIOD/2) 
+    	if (state != 3'b100) 
+    		begin
+        	$display("***FAIL 1***");    
+        	err=1;
+    	   end 
+        #CLK_PERIOD
+    	if (state != 3'b110)
+    		begin
+        	$display("***FAIL 2***");    
+        	err=1;
+    	   end 
+        #CLK_PERIOD
+    	if (state != 3'b001)
+    		begin
+        	$display("***FAIL 3***");    
+        	err=1;
+            end 
+        #CLK_PERIOD
+    	if (state != 3'b010)
+            begin
+        	$display("***FAIL 4***");    
+        	err=1;
+    	    end 
+        #CLK_PERIOD
+    	if (state != 3'b100)
+    		begin
+        	$display("***FAIL 5***");    
+        	err=1;
+    	    end 
+
+end     
+
+//Todo: Finish test, check for success
+	initial 
+	begin
+        #500 
+        if (err == 0)
+        	$display("***TEST PASSED!***");
+        $finish;
+    end
+
+//Todo: Instantiate counter module
+	trafficlight top ( 
+		.clk (clk),
+		.red (red),
+		.amber (amber),
+		.green (green)
+		);
+
+endmodule
